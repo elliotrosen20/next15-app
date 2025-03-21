dd621c619da913d466a8df7d6a3cbb1842d76b1a
Elliot Rosen <elliotrosen2@gmail.com>
2025-03-21 12:29:10 -0400

    Add navs and link functionality.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Navigation } from "./components/navigation";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,9 +29,15 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <header className="bg-slate-900 text-white p-4 text-center">
-          <p>Welcome to Next.js 15 Crash Course</p>
+          {/* <p>Demo Layout Header</p> */}
+          <Navigation />
         </header>
+
         {children}
+
+        <footer className="bg-slate-900 text-white p-4 text-center">
+          Elliot Rosen
+        </footer>
       </body>
     </html>
   );
